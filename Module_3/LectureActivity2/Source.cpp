eb50cab4779c5630e022074d303b12ae7783084a
SahajKhan <sahajkhan@gmail.com>
2021-06-09 23:24:46 -0400

    Update Source.cpp

--- Module_3/LectureActivity2/Source.cpp
+++ Module_3/LectureActivity2/Source.cpp
@@ -1,1 +1,30 @@
+// Header
+#include <iostream>
+#include <string>
+#include "TArray.h"
+using namespace std;
 
+int main() {
+
+	int numPlayers;
+	int value;
+
+	cout << "Enter number of players: ";
+	cin >> numPlayers;
+
+	TArray<int> myValues(numPlayers);
+
+	for (int i = 0; i < myValues.getSize(); i++) {
+		cout << "Enter a value: ";
+		cin >> value;
+		myValues.set(value,i);
+	}
+
+	for (int i = 0; i < myValues.getSize(); i++) {
+		cout << "value " << i << " = " <<
+		myValues.get(i) << endl;
+	}
+	
+
+	return 0;
+}
